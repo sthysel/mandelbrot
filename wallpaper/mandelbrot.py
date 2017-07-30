52862036cd9cdbcc4e7e0f9840db946561d1fa12
Thys Meintjes <sthysel@gmail.com>
2017-07-30 13:48:34 +0800

    house keeping

--- wallpaper/mandelbrot.py
+++ wallpaper/mandelbrot.py
@@ -11,7 +11,6 @@ import numpy as np
 from PIL import Image
 from numba import jit
 
-
 MAXITERS = 200
 RADIUS = 100
 
@@ -20,28 +19,28 @@ RADIUS = 100
 def color(z, i):
     v = np.log2(i + 1 - np.log2(np.log2(abs(z)))) / 5
     if v < 1.0:
-        return v**4, v**2.5, v
+        return v ** 4, v ** 2.5, v
     else:
-        v = max(0, 2-v)
-        return v, v**1.5, v**3
+        v = max(0, 2 - v)
+        return v, v ** 1.5, v ** 3
 
 
 @jit
 def iterate(c):
     z = 0j
     for i in range(MAXITERS):
-        if z.real*z.real + z.imag*z.imag > RADIUS:
+        if z.real * z.real + z.imag * z.imag > RADIUS:
             return color(z, i)
-        z = z*z + c
-    return 0, 0 ,0
+        z = z * z + c
+    return 0, 0, 0
 
 
 def main(xmin, xmax, ymin, ymax, width, height):
-    y, x = np.ogrid[ymax: ymin: height*1j, xmin: xmax: width*1j]
-    z = x + y*1j
+    y, x = np.ogrid[ymax: ymin: height * 1j, xmin: xmax: width * 1j]
+    z = x + y * 1j
     red, green, blue = np.asarray(np.frompyfunc(iterate, 1, 3)(z)).astype(np.float)
     img = np.dstack((red, green, blue))
-    Image.fromarray(np.uint8(img*255)).save('mandelbrot.png')
+    Image.fromarray(np.uint8(img * 255)).save('mandelbrot.png')
 
 
 if __name__ == '__main__':
